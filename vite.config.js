043e5e7a1d1856d1cf900697f16308ec13c37575
OmmYadav <idomyadavv@gmail.com>
2026-06-20 11:31:45 +0530

    Update base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Trading-app/', // Base path matches the GitHub Pages repository name
+  base: '/Trading-App/', // Base path matches the GitHub Pages repository name
   build: {
     outDir: 'dist'
   }
